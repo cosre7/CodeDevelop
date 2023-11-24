5f28181bee31a5f64d471154c88fd0da494de904
cosre7 <cosre7@gmail.com>
2023-11-24 14:01:11 +0900

    feat : 레파지토리 이동

---
+++ pyj/react-for-beginners/src/App.js
@@ -0,0 +1,21 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log('I run all the time');
+  const iRunOnlyOnce = () => {
+    console.log("i run only once.");
+  };
+  useEffect(iRunOnlyOnce, []);
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
+}
+
+export default App;
