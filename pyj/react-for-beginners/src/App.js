4a13e81c339a12c350f750b3199e6e8b89ef8df5
cosre7 <cosre7@gmail.com>
2023-11-24 14:20:30 +0900

    feat : 검색 input 추가

--- pyj/react-for-beginners/src/App.js
+++ pyj/react-for-beginners/src/App.js
@@ -4,13 +4,17 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log('I run all the time');
   useEffect(() => {
     console.log('CALL THE API');
   }, []);
+  console.log("search for", keyword);
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
