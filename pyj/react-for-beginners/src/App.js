d3282844860d8d1d3bde282fa744219e8fc285db
cosre7 <cosre7@gmail.com>
2023-11-24 14:02:41 +0900

    refactor : 익명함수로 useEffect 이용

--- pyj/react-for-beginners/src/App.js
+++ pyj/react-for-beginners/src/App.js
@@ -6,10 +6,9 @@ function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
   console.log('I run all the time');
-  const iRunOnlyOnce = () => {
-    console.log("i run only once.");
-  };
-  useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    console.log('CALL THE API');
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
