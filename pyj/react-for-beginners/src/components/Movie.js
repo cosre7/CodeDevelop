2b15cbd1129e5386b2e871918c6ea8297bcfe60d
cosre7 <cosre7@gmail.com>
2023-11-30 16:02:15 +0900

    feat : 노마드코더 reactJS 강의 마무리

--- pyj/react-for-beginners/src/components/Movie.js
+++ pyj/react-for-beginners/src/components/Movie.js
@@ -8,7 +8,7 @@ function Movie({id, coverImg, title, summary, genres}) {
             <h1>
                 <Link to={`/movie/${id}`}>{title}</Link>
             </h1>
-            <p>{summary}</p>
+            <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
             <ul>
                 {genres && genres.map((g) => (<li key={g}>{g}</li>))}
             </ul>
